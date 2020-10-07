77db9d81d5ce9a92172b169ea23e89fa14b901d7
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2020-10-07 21:12:49 +0630

    feat: Database Connected

--- server.js
+++ server.js
@@ -3,15 +3,29 @@ const bodyParser  = require('body-parser');
 const cors = require('cors');
 const mongoJs = require('mongojs');
 
+const uri = "mongodb+srv://edmandravers:edmandravers@edmandravers.cgvs0.mongodb.net/edm&ravers?retryWrites=true&w=majority";
+
+
+
+const db = mongoJs(uri, ["artists"]);
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.status(200).json({
-        data: {
-            message: "Success Bro...!"
+    db.artists.find((err, data)=>{
+        if(err){
+            res.status(500).json({
+                meta: "Internal Server Error."
+            })
+        }
+        else {
+            res.status(200).json({
+                meta: "Ok",
+                data: data
+            })
         }
     })
 });
