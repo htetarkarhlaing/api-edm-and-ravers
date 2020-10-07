c3263f241a665482f2b2ac9e2945d68398c82158
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2020-10-07 20:42:15 +0630

    feat: Express framework setup done

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log('Hello_world');
+const express = require('express');
+const bodyParser  = require('body-parser');
+const cors = require('cors');
+const mongoJs = require('mongojs');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        data: {
+            message: "Success Bro...!"
+        }
+    })
+});
+
+const PORT = 8000;
+
+app.listen( PORT, ()=> {
+    console.log(`localhost:${PORT}`);
+})
